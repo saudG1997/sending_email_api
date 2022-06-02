2acdeca6eaaa2afbcfb979144aef2e851d4698b4
lalit saud <lalitsaud1997@gmail.com>
2022-06-02 12:13:41 +0545

    email user and pass git ignore

--- sending_email/settings.py
+++ sending_email/settings.py
@@ -129,8 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SMTP Mail service with decouple
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'restapi94@gmail.com' #os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD ='restAPI@94%' #os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER') 
+EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_PASS')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
