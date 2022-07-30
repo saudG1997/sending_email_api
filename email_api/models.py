bae38ec64caa53ed803942e706887a29b61cc61c
lalit saud <lalitsaud1997@gmail.com>
2022-07-30 17:34:09 +0545

    lalit

--- email_api/models.py
+++ email_api/models.py
@@ -5,6 +5,5 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
-
     def __str__(self):
         return self.email
